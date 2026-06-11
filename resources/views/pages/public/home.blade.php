b11d7da24df888ccb8c248273303fd96d5746aa4
OdinZD <odin.perica@gmail.com>
2026-06-11 18:52:37 +0200

    Add judo action photo to homepage hero

--- resources/views/pages/public/home.blade.php
+++ resources/views/pages/public/home.blade.php
@@ -55,11 +55,11 @@
                 {{-- Right column --}}
                 <div class="relative animate-fade-up animate-stagger-2">
                     <div class="aspect-[4/3] rounded-2xl bg-bura-100 shadow-2xl shadow-bura-500/10 overflow-hidden">
-                        <div class="w-full h-full bg-gradient-to-br from-bura-200 to-bura-100 flex items-center justify-center">
-                            <svg class="size-20 text-bura-300" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909M3.75 21h16.5A2.25 2.25 0 0 0 22.5 18.75V5.25A2.25 2.25 0 0 0 20.25 3H3.75A2.25 2.25 0 0 0 1.5 5.25v13.5A2.25 2.25 0 0 0 3.75 21Z" />
-                            </svg>
-                        </div>
+                        <img
+                            src="{{ asset('images/hero.jpg') }}"
+                            alt="Judo Klub Bura - trening"
+                            class="w-full h-full object-cover"
+                        >
                     </div>
 
                     {{-- Floating badge --}}
