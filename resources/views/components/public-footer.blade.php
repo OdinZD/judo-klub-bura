535149cc60f5046020568bfeb80512efbdf0d9e6
OdinZD <odin.perica@gmail.com>
2026-06-11 18:24:28 +0200

    Adding new address for contact information

--- resources/views/components/public-footer.blade.php
+++ resources/views/components/public-footer.blade.php
@@ -29,7 +29,7 @@
                     </div>
                 </div>
                 <p class="text-white/60 text-sm leading-relaxed">
-                    Snaga jadranske bure. Treniramo judo za sve uzraste u modernom okruženju s naglaskom na disciplinu, poštovanje i zajedništvo.
+                    Snaga jadranske bure. Treniramo judo za sve uzraste u prijateljskom okruženju s naglaskom na disciplinu, poštovanje i zajedništvo.
                 </p>
             </div>
 
@@ -53,13 +53,13 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 0 1 15 0Z" />
                         </svg>
-                        <span class="text-white/60 text-sm">Sportska dvorana Bura<br>Obala 12, 21000 Split</span>
+                        <span class="text-white/60 text-sm"><br>23248, Ljubač</span>
                     </li>
                     <li class="flex items-center gap-3">
                         <svg class="size-5 text-bura-400 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                         </svg>
-                        <a href="tel:+385911234567" class="text-white/60 hover:text-bura-400 transition-colors text-sm">+385 91 123 4567</a>
+                        <a href="tel:+4915255317924" class="text-white/60 hover:text-bura-400 transition-colors text-sm">+4915255317924</a>
                     </li>
                     <li class="flex items-center gap-3">
                         <svg class="size-5 text-bura-400 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
